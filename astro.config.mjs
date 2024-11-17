e7978d725bb763366b49ebaac39db7007e24f27c
Sven Gatter <sven@invernis.com>
2024-11-17 23:08:07 +0100

    change site to invernis.com

--- astro.config.mjs
+++ astro.config.mjs
@@ -5,6 +5,6 @@ import tailwind from '@astrojs/tailwind';
 // https://astro.build/config
 export default defineConfig({
   integrations: [tailwind()],
-  site: 'https://invernis.github.io',
+  site: 'https://invernis.com',
   base: 'invernis.github.io',
 });
